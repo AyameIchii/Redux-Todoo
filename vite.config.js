bf92736bf5090a209e9be002103e5a38b59676b2
Phu Khanh <khanhphuhoiquy@gmail.com>
2026-02-07 11:01:43 +0700

    web

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Redux-Todoo/'   // 👈 dòng QUAN TRỌNG NHẤT
 })
